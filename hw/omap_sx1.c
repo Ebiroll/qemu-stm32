Ebiroll/qemu-stm32

commit 69a510991f3e4a99692f879f4a8981030e45d5ac

graphical_console_init change (Stefano Stabellini)
Patch 5/7

This patch changes the graphical_console_init function to return an
allocated DisplayState instead of a QEMUConsole.

This patch contains just the graphical_console_init change and few other
modifications mainly in console.c and vl.c.
It was necessary to move the display frontends (e.g. sdl and vnc)
initialization after machine->init in vl.c.

This patch does *not* include any required changes to any device, these
changes come with the following patches.

Patch 6/7

This patch changes the QEMUMachine init functions not to take a
DisplayState as an argument because is not needed any more;

In few places the graphic hardware initialization function was called
only if DisplayState was not NULL, now they are always called.
Apart from these cases, the rest are all mechanical substitutions.

Patch 7/7

This patch updates the graphic device code to use the new
graphical_console_init function.

As for the previous patch, in few places graphical_console_init was called
only if DisplayState was not NULL, now it is always called.
Apart from these cases, the rest are all mechanical substitutions.

Signed-off-by: Stefano Stabellini <stefano.stabellini@eu.citrix.com>
Signed-off-by: Anthony Liguori <aliguori@us.ibm.com>



git-svn-id: svn://svn.savannah.nongnu.org/qemu/trunk@6344 c046a42c-6fe2-441c-8c8c-71466251a162

hw/omap_sx1.c
@@ -136,7 +136,7 @@ static void sx1_init(ram_addr_t ram_size, int vga_ram_size,
         flash_size = flash2_size;
     }
 
-    cpu = omap310_mpu_init(sx1_binfo.ram_size, ds, cpu_model);
+    cpu = omap310_mpu_init(sx1_binfo.ram_size, cpu_model);
 
     /* External Flash (EMIFS) */
     cpu_register_physical_memory(OMAP_CS0_BASE, flash_size,
@@ -201,8 +201,7 @@ static void sx1_init(ram_addr_t ram_size, int vga_ram_size,
         cpu->env->regs[15] = 0x00000000;
     }
 
-    ds->surface = qemu_resize_displaysurface(ds->surface, 640, 480, 32, 4 * 640);
-    dpy_resize(ds);
+    qemu_console_resize(ds, 640, 480);
 }
 
 static void sx1_init_v1(ram_addr_t ram_size, int vga_ram_size,

commit 1fbfab5f0578457ba10aa5c21d369f776956b925

Fix bug in omap_sx1.c introduced by r6344
This patch changes the QEMUMachine init function not to take a
DisplayState as an argument because is not needed any more;

Signed-off-by: Stefan Weil <weil@mail.berlios.de>
Signed-off-by: Aurelien Jarno <aurelien@aurel32.net>

git-svn-id: svn://svn.savannah.nongnu.org/qemu/trunk@6552 c046a42c-6fe2-441c-8c8c-71466251a162

hw/omap_sx1.c
@@ -116,7 +116,7 @@ static struct arm_boot_info sx1_binfo = {
 };
 
 static void sx1_init(ram_addr_t ram_size, int vga_ram_size,
-                const char *boot_device, DisplayState *ds,
+                const char *boot_device,
                 const char *kernel_filename, const char *kernel_cmdline,
                 const char *initrd_filename, const char *cpu_model,
                 const int version)
@@ -201,24 +201,25 @@ static void sx1_init(ram_addr_t ram_size, int vga_ram_size,
         cpu->env->regs[15] = 0x00000000;
     }
 
-    qemu_console_resize(ds, 640, 480);
+    /* TODO: fix next line */
+    //~ qemu_console_resize(ds, 640, 480);
 }
 
 static void sx1_init_v1(ram_addr_t ram_size, int vga_ram_size,
-                const char *boot_device, DisplayState *ds,
+                const char *boot_device,
                 const char *kernel_filename, const char *kernel_cmdline,
                 const char *initrd_filename, const char *cpu_model)
 {
-    sx1_init(ram_size, vga_ram_size, boot_device, ds, kernel_filename,
+    sx1_init(ram_size, vga_ram_size, boot_device, kernel_filename,
                 kernel_cmdline, initrd_filename, cpu_model, 1);
 }
 
 static void sx1_init_v2(ram_addr_t ram_size, int vga_ram_size,
-                const char *boot_device, DisplayState *ds,
+                const char *boot_device,
                 const char *kernel_filename, const char *kernel_cmdline,
                 const char *initrd_filename, const char *cpu_model)
 {
-    sx1_init(ram_size, vga_ram_size, boot_device, ds, kernel_filename,
+    sx1_init(ram_size, vga_ram_size, boot_device, kernel_filename,
                 kernel_cmdline, initrd_filename, cpu_model, 2);
 }
 